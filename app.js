6bf582c8d4b6316d2f49cfe4af6fcc735e091ae6
Adrian <reneavila1993@gmail.com>
2021-02-12 23:20:52 -0700

    Initialize app, install all necessary depencies except JWT, connect to MongoDB

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Enable CORS
+app.use(cors());
+
+//ROOT Route
+app.get('/', (req, res) => {
+    res.send("Welcome to the back-end of Holocron Keeper");
+});
+
+//Connect to MongoDB Database
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    () => console.log("Successful database connection")
+);
+
+//Listen to port 9000
+app.listen(9000);
