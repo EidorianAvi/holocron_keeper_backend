84d19082c9da7cdd76d3737948541ac9af404272
Adrian <reneavila1993@gmail.com>
2021-02-20 23:24:01 -0700

    Sample code to try and replicae

--- routes/novels.js
+++ routes/novels.js
@@ -5,6 +5,15 @@ const multer = require('multer');
 const fs = require('fs');
 require('dotenv/config');
 
+app.post('/api/images', parser.single("image"), (req, res) => {
+    console.log(req.file) // to see what is returned to you
+    const image = {};
+    image.url = req.file.url;
+    image.id = req.file.public_id;
+    Image.create(image) // save image information in database
+      .then(newImage => res.json(newImage))
+      .catch(err => console.log(err));
+  });
 
 //Get all Novels
 router.get('/', async (req, res) => {
